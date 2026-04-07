e677a121862ef0df9f8b140ba1305ed32b263369
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2026-04-07 12:54:17 -0700

    Update

---
+++ Week1/HelloWorld.java
@@ -0,0 +1,23 @@
+//This is a comment
+//Comments are ignored by the compiler
+//They are notes for you or other developers
+
+
+/*
+ * This
+ * is a multi
+ * line
+ * comment
+ */
+
+public class HelloWorld {//This is the start of the class
+
+	//This is the main method - When you run your application
+	//this is what runs
+	public static void main(String[] args) {//This is the start of main
+		
+		
+	}//This is the end of main
+	
+	
+}//This is the end of the class
