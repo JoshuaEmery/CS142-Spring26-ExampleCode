08d9393c88556287eb4c1c302decc8c9a672c10e
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-05-19 14:12:49 -0700

    Update IntroToNestedLoops.java

--- Week7/IntroToNestedLoops.java
+++ Week7/IntroToNestedLoops.java
@@ -4,6 +4,7 @@ public class IntroToNestedLoops {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		printIandJ();
+		printStars();
 	}
 	static void printIandJ() {
 		//when one loop is nested within another
@@ -16,6 +17,21 @@ public class IntroToNestedLoops {
 			for(int j = 0; j < 3; j++) {
 				//inner loop does the work
 				System.out.print("[i: " + i + ", j: " + j + "], ");
+			}
+			//the outer loop moves to a new line
+			System.out.println();
+		}
+	}
+	//take in parameters for rows and cols
+	//change printing of the shape based on rows and cols
+	static void printStars() {
+		System.out.println("Print stars example");
+		//outer loop - constrains the number of rows
+		for(int i = 0; i < 5; i++) {
+			//inner loop - the number of columns
+			for(int j = 0; j < 3; j++) {
+				//inner loop does the work
+				System.out.print("* ");
 			}
 			//the outer loop moves to a new line
 			System.out.println();
