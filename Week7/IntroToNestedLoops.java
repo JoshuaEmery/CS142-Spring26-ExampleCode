eaa7307409f3911ae69d9c4896200afbf0e5290e
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-05-19 13:59:30 -0700

    Update IntroToNestedLoops.java

--- Week7/IntroToNestedLoops.java
+++ Week7/IntroToNestedLoops.java
@@ -1,6 +1,25 @@
-public class IntroToNestedLoops {
-    //When a loop is nested within another loop the inner loop
-    //runs in its entirety for each interation of the outer loop
 
+public class IntroToNestedLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printIandJ();
+	}
+	static void printIandJ() {
+		//when one loop is nested within another
+		//the inner loops runs in its entirety for each
+		//iteration of the outer loop
+		System.out.println("Print I and J Example");
+		//outer loop
+		for(int i = 0; i < 5; i++) {
+			//inner loop runs 5 * 3 times
+			for(int j = 0; j < 3; j++) {
+				//inner loop does the work
+				System.out.print("[i: " + i + ", j: " + j + "], ");
+			}
+			//the outer loop moves to a new line
+			System.out.println();
+		}
+	}
 
 }
